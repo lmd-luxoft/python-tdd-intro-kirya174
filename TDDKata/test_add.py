aa2d7d829763ce382a38af6bcd5c3c3c6e7a8834
Kirill <kirya174@gmail.com>
2022-04-21 10:41:26 +0300

    first commit

--- TDDKata/test_add.py
+++ TDDKata/test_add.py
@@ -1,5 +1,6 @@
 from add import add
 
+
 # "2,3,1" => 6
 def test_2_3_1_should_be_6():
     # arrange
@@ -12,3 +13,38 @@ def test_2_3_1_should_be_6():
     # assert
     assert expected == actual
 
+
+def test_empty_string_returns_error():
+    # arrange
+    expression = " ", 3
+    expected = -1
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
+
+
+def test_none_returns_error():
+    # arrange
+    expression = None, 2, 5
+    expected = -1
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
+
+
+def test_add_5_and_minus_1_shhould_be_4():
+    # arrange
+    expression = 5, -1
+    expected = 4
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
