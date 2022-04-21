281de16b0832a05ad0252377caf13995993cdebd
Kirill <kirya174@gmail.com>
2022-04-21 15:11:30 +0300

    function update

--- TDDKata/test_add.py
+++ TDDKata/test_add.py
@@ -14,6 +14,18 @@ def test_2_3_1_should_be_6():
     assert expected == actual
 
 
+def test_add_5_and_minus_1_should_be_4():
+    # arrange
+    expression = "5,-1"
+    expected = 4
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
+
+
 def test_empty_string_returns_error():
     # arrange
     expression = " , 3"
@@ -38,10 +50,34 @@ def test_none_returns_error():
     assert expected == actual
 
 
-def test_add_5_and_minus_1_should_be_4():
+def test_semicolon_separator():
     # arrange
-    expression = "5, -1"
-    expected = 4
+    expression = "2.3;4.5;6;7"
+    expected = 19.8
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
+
+
+def test_single_prefix_as_separator():
+    # arrange
+    expression = "//#\n1#3#4#5"
+    expected = 13
+
+    # act
+    actual = add(expression)
+
+    # assert
+    assert expected == actual
+
+
+def test_multiple_prefixes_as_separator():
+    # arrange
+    expression = "//#$@\n1#2$3@4@5"
+    expected = 15
 
     # act
     actual = add(expression)
